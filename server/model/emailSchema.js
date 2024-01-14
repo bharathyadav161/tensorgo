141256e462a0601cf998c3aae868657f2d8a2c80
Bharath Kumar Polaboina <97655326+bharathyadav161@users.noreply.github.com>
2024-01-15 00:31:11 +0530

    Delete server directory

--- server/model/emailSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const emailSchema = new mongoose.Schema({
-    to: String,
-    subject: String,
-    body: String,
-  });
-  
-  var Email = mongoose.model('Email', emailSchema);
-  module.exports = Email;
