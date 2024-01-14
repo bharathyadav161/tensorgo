2b05d5c0a936148a01433f9e46624597bb84f78c
Bharath Kumar Polaboina <97655326+bharathyadav161@users.noreply.github.com>
2024-01-15 00:36:38 +0530

    Update Footer.js

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -7,7 +7,7 @@ import './footer.css';
 export const Footer = () => {
   return (
     <footer className="footer-container">
-      <p>&copy; 2024 PostConnect Hub. All rights reserved.</p>
+      <p>&copy; 2024  EmailHub. All rights reserved.</p>
     </footer>
   );
 };
